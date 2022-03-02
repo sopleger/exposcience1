0823d2cd2bf6dc8921dd4c4770bd7ab5f59c4cec
sopleger <97857439+sopleger@users.noreply.github.com>
2022-03-02 10:28:54 -0500

    Update predict_stroke2.py

--- predict_stroke2.py
+++ predict_stroke2.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def load_model() : 
-    with open("avc_complete_5.pkl", "rb") as file :
+    with open("avc_complete_5.pkl.zip", "rb") as file :
         data = pickle.load(file)
     return data
 
